b4c0c89becf5769df01769306a0fd0dd8e5267f2
Mateus Diniz <serv.mateus@gmail.com>
2024-02-02 16:32:54 -0300

    chore: modificar modelo

--- core/bookstore/models/Order.py
+++ core/bookstore/models/Order.py
@@ -6,3 +6,4 @@ from django.contrib.auth.models import User
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
