f10f201db531ca555d38e067e4669e125050b048
Mateus Diniz <serv.mateus@gmail.com>
2024-02-02 14:21:01 -0300

    chore: reconfig de modelos

--- core/bookstore/models/Order.py
+++ core/bookstore/models/Order.py
@@ -5,7 +5,4 @@ from django.contrib.auth.models import User
 
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-
-    def __str__(self):
-        return self.user.username
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- core/bookstore/models/Product.py
+++ core/bookstore/models/Product.py
@@ -4,9 +4,6 @@ from .Category import Category
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=500, blank=True, null=True)
-    price = models.FloatField(null=False)
+    price = models.FloatField(null=True)
     active = models.BooleanField(default=True)
     category = models.ManyToManyField(Category, blank=True)
-
-    def __str__(self):
-        return self.title
