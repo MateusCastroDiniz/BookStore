6bcaafdbbd13d009b03cc72f320c507eb508b57f
Mateus Diniz <serv.mateus@gmail.com>
2024-02-02 16:49:30 -0300

    uploads

---
+++ core/bookstore/serializers/category_serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from core.bookstore.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = [
+            'title',
+            'slug',
+            'description',
+            'active',
+        ]
+        extra_kwargs = {'slug': {'required': False}}
