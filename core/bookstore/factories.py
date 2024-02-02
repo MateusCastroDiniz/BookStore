57a001a56082cc21a687464d8147298e3b307149
Mateus Diniz <serv.mateus@gmail.com>
2024-02-02 17:12:01 -0300

    chore: modificando factories para testes

---
+++ core/bookstore/factories.py
@@ -0,0 +1,58 @@
+import factory
+
+from django.contrib.auth.models import User
+from .models.Product import Product
+from .models.Order import Order
+from .models.Category import Category
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    username = factory.Faker('pystr')
+    email = factory.LazyAttribute(lambda x: '%s@exemple.com' % x.username)
+
+    class Meta:
+        model = User
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('pystr')
+    slug = factory.Faker('pystr')
+    description = factory.Faker('pystr')
+    active = factory.Iterator([True, False])
+
+    class Meta:
+        model = Category
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('pystr')
+    price = factory.Faker('pyfloat')
+    category = factory.LazyAttribute(CategoryFactory)
+
+    @factory.post_generation
+    def category(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for category in extracted:
+                self.category.add(category)
+
+    class Meta:
+        model = Product
+        skip_postgeneration_save = True
+
+
+class OrderFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+
+    @factory.post_generation
+    def product(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            for product in extracted:
+                self.product.add(product)
+
+    class Meta:
+        model = Order
+        skip_postgeneration_save = True
