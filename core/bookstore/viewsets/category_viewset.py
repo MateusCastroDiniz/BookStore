fce76eb590e67abcb5268bf85162206ef99f907e
Mateus Diniz <serv.mateus@gmail.com>
2024-02-02 19:47:32 -0300

    fix: corrigir importação conflitante

--- core/bookstore/viewsets/category_viewset.py
+++ core/bookstore/viewsets/category_viewset.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from core.bookstore.models import Category
-from core.bookstore.serializers import CategorySerializer
+from core.bookstore.serializers.category_serializer import CategorySerializer
 
 
 class CategoryViewSet(ModelViewSet):
--- core/bookstore/viewsets/product_viewset.py
+++ core/bookstore/viewsets/product_viewset.py
@@ -2,7 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from core.bookstore.models import Product
 from core.bookstore.serializers import ProductSerializer
 
-
 class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
