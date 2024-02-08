0b49f7677740f2fc36a6f1e947db3f462d4b8c19
Mateus Diniz <serv.mateus@gmail.com>
2024-02-08 13:58:22 -0300

    chore: Adicionar autenticação via token em cada viewset

--- core/bookstore/viewsets/product_viewset.py
+++ core/bookstore/viewsets/product_viewset.py
@@ -1,7 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from core.bookstore.models import Product
 from core.bookstore.serializers import ProductSerializer
 
+
 class ProductViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().order_by('-id')
