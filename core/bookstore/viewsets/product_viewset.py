5e0ae17378ab5e4a5aa3f7d60f1ed6b307aa809c
Mateus Diniz <serv.mateus@gmail.com>
2024-02-08 14:09:51 -0300

    fix: Remover campo de autenticação do VS product

--- core/bookstore/viewsets/product_viewset.py
+++ core/bookstore/viewsets/product_viewset.py
@@ -1,12 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from core.bookstore.models import Product
 from core.bookstore.serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
     queryset = Product.objects.all().order_by('-id')
