940c2460ebd342a4beec21986445bd041c066340
Mateus Diniz <serv.mateus@gmail.com>
2024-02-22 12:06:16 -0300

    fix: corrigir falha de integração com servidor

--- core/settings.py
+++ core/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-h=#aq9iakl-tg#d#%pm=&4&dgw^u@fpopaleb8^1k9b5dvow2w'
+SECRET_KEY = 'django-insecure-h=#aq9iakl-tg#d#%pm=&4&dgw^u@fpopaleb8^1k9b5dvow2w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -147,8 +147,8 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
