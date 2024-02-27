6980738cc57a11318cf90b77853a6b249b643c17
Mateus Diniz <serv.mateus@gmail.com>
2024-02-27 18:48:14 -0300

    remoção de campo slug

--- core/bookstore/models/category.py
+++ core/bookstore/models/category.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    # slug = models.SlugField(unique=True)
     description = models.TextField(max_length=200, blank=True, null=True)
     active = models.BooleanField(default=True)
 
